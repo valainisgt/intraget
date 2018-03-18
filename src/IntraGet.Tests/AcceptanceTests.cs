90d2aa6cb5081124a88915020f67cbad0769daee
Greg Valainis <valainisgt@gmail.com>
2018-03-18 17:12:51 -0400

    Added main project and test project

---
+++ src/IntraGet.Tests/AcceptanceTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.Owin.Testing;
+using Moq;
+
+namespace IntraGet.Tests
+{
+    public class AcceptanceTests
+    {
+        private static void SetupServer(Owin.IAppBuilder app)
+        {
+            IntraGet.Startup.ConfigureMiddleware(app);
+        }
+        [Fact]
+        public void GetReturnsResponse()
+        {
+            using (var sut = TestServer.Create(SetupServer))
+            {
+                var response = sut.HttpClient.GetAsync("/").Result;
+
+                Assert.IsType<System.Net.Http.HttpResponseMessage>(response);
+            }
+        }
+        [Fact]
+        public void GetReturnsFailureWithBadCredentials()
+        {
+            var authServiceStub = new Mock<IntraGet.IIntraGetAuthenticationService>();
+            authServiceStub.Setup(s => s.Authenticate(It.IsAny<System.Security.Principal.IPrincipal>())).Returns(false);
+            using (var sut = TestServer.Create(SetupServer))
+            {
+                var response = sut.HttpClient.GetAsync("/").Result;
+                Assert.False(response.IsSuccessStatusCode);
+            }
+        }
+        [Fact]
+        public void GetReturnsSuccessWithGoodCredentials()
+        {
+            var authServiceStub = new Mock<IntraGet.IIntraGetAuthenticationService>();
+            authServiceStub.Setup(s => s.Authenticate(It.IsAny<System.Security.Principal.IPrincipal>())).Returns(true);
+            using (var sut = TestServer.Create(SetupServer))
+            {
+                var response = sut.HttpClient.GetAsync("/").Result;
+                Assert.True(response.IsSuccessStatusCode);
+            }
+        }
+    }
+}
